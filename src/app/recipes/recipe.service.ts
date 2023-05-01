003c77c6d688e6c8b588fa3ef6c6dc936b2b481f
Pedro Amaral <amaralph1995@gmail.com>
2023-05-01 19:15:24 -0300

    late initial commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from "@angular/core";
+
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('Hamburguer',
+      'This is a simply hamburguer',
+      'https://classic.exame.com/wp-content/uploads/2020/05/mafe-studio-LV2p9Utbkbw-unsplash-1.jpg?quality=70&strip=info&w=1024',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 2)
+      ]),
+    new Recipe('Pizza',
+      'This is a pizza',
+      'https://img.itdg.com.br/tdg/images/blog/uploads/2022/07/5-itens-necessarios-para-se-tornar-um-pizzaiolo-neste-Dia-da-Pizza.jpg?mode=crop&width={:width=%3E150,%20:height=%3E130}',
+      [
+        new Ingredient('Dough', 1),
+        new Ingredient('Calabresa', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+}
